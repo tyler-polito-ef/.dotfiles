79a742c5feae270babb7707bbd5704b6879e7caf
tpolito <things@tpolito.com>
2024-06-10 22:21:24 -0500

    add WIP nvim config

---
+++ nvim/lua/options.lua
@@ -0,0 +1,14 @@
+-- Set vim options
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.clipboard = "unnamedplus"
+opt.ignorecase = true
+opt.smartcase = true 
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.termguicolors = true
+opt.mouse = "a"
+opt.hlsearch = false
