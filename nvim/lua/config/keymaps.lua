2eef997f399c34d6034b83151a44af0ae1aecd33
tpolito <things@tpolito.com>
2024-08-13 08:20:19 -0500

    neovim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Format file
+vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format" })
+
+vim.keymap.set("n", "<leader>w", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
