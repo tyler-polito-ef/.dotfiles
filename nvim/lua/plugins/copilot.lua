1691970031406b09ca40cf96ffcb91def5b19cd4
tpolito <things@tpolito.com>
2024-08-15 21:21:46 -0500

    copilot updates

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "<C-y>" } },
+    filetypes = {
+      markdown = false,
+    },
+  },
+}
--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -3,5 +3,8 @@ return {
   -- disable neo tree
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- disable which key
-  {"folke/which-key.nvim", enabled = false},
+  { "folke/which-key.nvim", enabled = false },
+  -- disable copilot completions inside dropdown menu
+  { "zbirenbaum/copilot-cmp", enabled = false },
 }
+
