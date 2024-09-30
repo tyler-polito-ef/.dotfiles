a4225cbad757df5828e5c4a38e2aac9338280b2f
Tyler Polito <tyler.polito@ef.com>
2024-09-30 11:17:06 -0500

    update nvim config

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<M-C-h>", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+    { "<M-C-l>", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+  },
+}
